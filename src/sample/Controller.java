049a7f33306ac187fe0b39c2e8f462b1d4cb92cd
MacTambo <58809620+MacTambo@users.noreply.github.com>
2022-02-02 10:32:12 +0100

    connection working fine

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,23 +4,17 @@ import connectDb.Connect;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Observable;
 
 import java.awt.event.ActionEvent;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 
 //84
 public class Controller {
@@ -141,6 +135,103 @@ public class Controller {
     @FXML
     void saveClient(MouseEvent event) {
 
+
+        txt_nom.setStyle("-fx-border-color: none");
+        txt_prenom.setStyle("-fx-border-color: none ");
+        txt_adresse.setStyle("-fx-border-color: none ");
+        txt_codep.setStyle("-fx-border-color: none ");
+        txt_ville.setStyle("-fx-border-color: none ");
+
+        String nom = txt_nom.getText();
+        String nomToTest = nom.toLowerCase();
+        if (nom.isEmpty()) {
+            txt_nom.setStyle("-fx-border-color: #ff0000 ; -fx-border-width: 2px");
+        }
+        String prenom = txt_prenom.getText();
+        if (prenom.isEmpty()) {
+            txt_prenom.setStyle("-fx-border-color: red ; -fx-border-width: 2px");
+        }
+        String adresse = txt_adresse.getText();
+        if (adresse.isEmpty()) {
+            txt_adresse.setStyle("-fx-border-color: red ; -fx-border-width: 2px");
+        }
+        String codePostal = txt_codep.getText();
+        if (codePostal.isEmpty()) {
+            txt_codep.setStyle("-fx-border-color: red ; -fx-border-width: 2px");
+        }
+        String ville = txt_ville.getText();
+        if (ville.isEmpty()) {
+            txt_ville.setStyle("-fx-border-color: red ; -fx-border-width: 2px");
+        }
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/skiloc", "root", "toor")) {
+
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT nom FROM clients");
+            ArrayList<String> names = new ArrayList<>();
+            while (result.next()) {
+                names.add(result.getString("nom").toLowerCase());
+            }
+
+            if (names.contains(nomToTest)) {
+                txt_nom.setStyle("-fx-text-fill: red; -fx-border-color: red ; -fx-border-width: 2px");
+                Alert alert2 = new Alert(Alert.AlertType.WARNING);
+                alert2.setTitle("Attention");
+                alert2.setHeaderText("Ce client existe déjà dans la base !");
+                alert2.showAndWait();
+                nom = "";
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Integer id = 0;
+
+        if (nom.isEmpty() | prenom.isEmpty() | adresse.isEmpty() | codePostal.isEmpty() | ville.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Attention");
+            alert.setHeaderText("Merci de ne laisser aucun champ client vide.");
+            alert.showAndWait();
+        } else {
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/skiloc", "root", "toor")) {
+
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery("SELECT id FROM clients");
+
+                while (result.next()) {
+                    id = result.getInt("id");
+                }
+                System.out.println(id);
+                id++;
+                String idStr = id.toString();
+
+                PreparedStatement pstmt = connection.prepareStatement("INSERT INTO clients(id,nom,prenom,adresse,code_postal,ville) VALUES (?,?,?,?,?,?)");
+
+                pstmt.setString(1, idStr);
+                pstmt.setString(2, nom);
+                pstmt.setString(3, prenom);
+                pstmt.setString(4, adresse);
+                pstmt.setString(5, codePostal);
+                pstmt.setString(6, ville);
+                pstmt.executeUpdate();
+                Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+                alert2.setTitle("Confirmation");
+                alert2.setHeaderText("Client ajouté à la base de données SkiLoc !");
+                alert2.showAndWait();
+                System.out.println("client ajouté");
+
+                txt_nom.setText("");
+                txt_prenom.setText("");
+                txt_adresse.setText("");
+                txt_codep.setText("");
+                txt_ville.setText("");
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @FXML
